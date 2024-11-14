7cf17f78d962ed731c816154439b85cc9b8df1dd
Asadbek <asadbekov.a@yandex.ru>
2024-11-14 12:26:14 +0300

    Изменения в test.sh в ветке conflict

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "#!/bin/bash" > test.sh
-echo "echo 'Hello Netology from dev branch!'" >> test.sh
+echo "echo 'Hello Netology, smth went wrong from dev branch!'" >> test.sh
 echo "echo 'Updating test.sh in dev branch'" >> test.sh
