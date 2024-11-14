74691d55f01cb75a00d61b56b9917b48ea30a05f
Asadbek <asadbekov.a@yandex.ru>
2024-11-14 12:29:15 +0300

    Изменения в test.sh в основной ветке

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 echo "#!/bin/bash" > test.sh
-echo "echo 'Hello Netology from dev branch!'" >> test.sh
+echo "echo 'Something is not working here!'" >> test.sh
 echo "echo 'Updating test.sh in dev branch'" >> test.sh
